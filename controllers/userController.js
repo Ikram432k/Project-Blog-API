950a5341299b1085ad27c06c35bc6fbc532b451b
ikram mohideen <ikram432k@gmail.com>
2022-10-20 20:31:49 +0530

    added controller for user

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require("../modals/usermodal");
+
+exports.userSignin_get =(req,res)=> {
+    res.send("not implemented");
+}
+
+exports.userSignin_post =(req,res)=> {
+    res.send("not implemented");
+}
+
+exports.userLogin_get =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.userLogin_post =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.userLogout =(req,res)=>{
+    res.send("not implemented");
+}
