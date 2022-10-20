5279c7a6f2fbc1c7c860a6c9d3a0aae45c3daa5a
ikram mohideen <ikram432k@gmail.com>
2022-10-20 20:31:15 +0530

    added controller of commnet

---
+++ controllers/commentController.js
@@ -0,0 +1,17 @@
+const Comment = require("../modals/commentmodal");
+
+exports.newComment_get =(req,res)=>{
+    res.send("not implemented")
+}
+
+exports.newComment_post = (req,res)=>{
+    res.send("not impelmented");
+}
+
+exports.commnetList =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.commentDelete =(req,res)=>{
+    res.send("not implemented");
+}
