b589d8b4b882c1a79d03aa0f1667f60e02873f85
ikram mohideen <ikram432k@gmail.com>
2022-10-20 20:31:31 +0530

    added controller for post

---
+++ controllers/postController.js
@@ -0,0 +1,17 @@
+const Post = require("../modals/postmodal");
+
+exports.createPost_get =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.createPost_post =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.postList =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.postVisibility =(req,res)=>{
+    res.send("not implemented");
+}
