1f4fd620ff7b21ed4e8840810f0522b3179d7264
ikram mohideen <ikram432k@gmail.com>
2022-10-21 21:15:59 +0530

    updated comment modal

--- modals/commentmodal.js
+++ modals/commentmodal.js
@@ -3,6 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    comment:{type: String,required: true}
+    user:{type: String,required: true},
+    comment:{type: String,required: true},
+    postId:{type: String,required: true},
+    date:{type: Date,default: Date.now}
+});
+commentSchema.virtual("date_formated").get(()=>{
+    return this.date.toLocaleDateString("en-gb",{
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minutes: "2-digit",
+    });
 });
 module.exports = mongoose.model("Comments",commentSchema);
