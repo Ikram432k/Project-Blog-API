1da78b7084b65b1289f16289bc136a34ceb6a2f6
ikram mohideen <ikram432k@gmail.com>
2022-10-22 22:42:42 +0530

    updated user model

--- modals/usermodal.js
+++ modals/usermodal.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-    username:{type: String, required: true},
+    username: {type: String, required: true, maxLength: 20},
     password:{type: String, required: true}
 })
 
