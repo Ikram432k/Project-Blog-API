5496c4a7f4fd503c17b8ec442788f970f2170eb3
ikram mohideen <ikram432k@gmail.com>
2022-10-23 20:58:47 +0530

    updated user model

--- modals/usermodal.js
+++ modals/usermodal.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = Schema({
     username: {type: String, required: true, maxLength: 20},
-    password:{type: String, required: true}
+    password:{type: String, required: true},
+    posts:[{type: Schema.Types.ObjectId, ref: 'Post'}]
 })
 
 module.exports = mongoose.model("User",userSchema);
