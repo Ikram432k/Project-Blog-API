2040726b597f88115540d37a03652005e1222a78
ikram mohideen <ikram432k@gmail.com>
2022-10-23 20:58:24 +0530

    updated comment model

--- modals/commentmodal.js
+++ modals/commentmodal.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    user:{type: String,required: true},
     comment:{type: String,required: true},
-    postId:{type: String,required: true},
+    postId:{type: Schema.Types.ObjectId,ref:"Post"},
     date:{type: Date,default: Date.now}
 });
 commentSchema.virtual("date_formated").get(()=>{
