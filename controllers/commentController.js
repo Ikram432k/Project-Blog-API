0a0c7095decc41ae5e3a7cea4f07c448c978ba77
ikram mohideen <ikram432k@gmail.com>
2022-10-23 20:57:31 +0530

    updated comment controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,15 +1,51 @@
+const { body,validationResult } = require("express-validator");
+const Post = require("../modals/postmodal");
 const Comment = require("../modals/commentmodal");
 
-exports.newComment_get =(req,res)=>{
-    res.send("not implemented")
-}
+exports.createComment = [
+    body('comment').trim().isLength({min:1}).withMessage("commnet must not be empty"),
+    async(req,res,next)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty){
+            res.status(403).json({
+                error:errors.array(),
+                data: req.body
+            });
+        }
+        try{
+            const comment = new Comment({
+                comment: req.body.comment,
+                // user: req.user._id,
+                postId: req.params.postId
+            })
+            comment.save(err=>{
+                if(err){
+                    return next(err);
+                }
+                res.status(200).json({message:"comment saved",comment});    
+            })
+            await Post.findOneAndUpdate(
+                {_id: req.params.postid},
+                {$push: {comments: comment}}
+            )
+        }catch(error){
+            res.status(403).json({error});
+        }
+    }
+];
 
-exports.newComment_post = (req,res)=>{
-    res.send("not impelmented");
-}
-
-exports.commnetList =(req,res)=>{
-    res.send("not implemented");
+exports.commentsOnpost = async(req,res,next)=>{
+    try{
+        // const comments = await Comment.find({},{title: 1, text: 1, timestamp: 1 })
+        // .populate('author', {username: 1, _id: 0})
+        const comments = await Comment.find({postId: req.params.postid})
+        if(!comments || comments.length==0){
+            return res.status(403).json({message: "no comments available"})
+        }
+        return res.status(200).json(comments)
+    }catch(error){
+        return next(error);
+    }
 }
 
 exports.commentDelete =(req,res)=>{
