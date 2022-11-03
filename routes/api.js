90a3eebf39c390f4f7efa92a46a6d7ab06ddd4ed
ikram mohideen <ikram432k@gmail.com>
2022-11-03 22:04:15 +0530

    updated api routes

--- routes/api.js
+++ routes/api.js
@@ -24,33 +24,33 @@ router.post("/signup", User.signUp);
 /************ CURD opertion of Post **************/
 
 // view all posts
-router.get("/posts",Post.postList); //ok
+router.get("/posts",Post.postList); 
 
 // view single post only
-router.get("/posts/:postid",Post.singlePost);//ok
+router.get("/posts/:postid",Post.singlePost);
 
 // view users post only
-router.get("/posts/:userid/userposts",Post.postOfUser);//ok
+router.get("/posts/:userid/userposts",Post.postOfUser);
 
 // create post for author - api/post
-router.post('/posts/createpost', passport.authenticate('jwt', {session: false}), Post.createPost);//ok
+router.post('/posts/createpost', passport.authenticate('jwt', {session: false}), Post.createPost);
 
 // updated single post
-router.post('/posts/:postid/update',passport.authenticate('jwt', {session: false}), Post.singlePostUpdated);//ok
+router.post('/posts/:postid/update',passport.authenticate('jwt', {session: false}), Post.singlePostUpdated);
 
 // delete single post
-router.post('/posts/:postid/delete',passport.authenticate('jwt', {session: false}),Post.DeleteSinglePost);//ok
+router.post('/posts/:postid/delete',passport.authenticate('jwt', {session: false}),Post.DeleteSinglePost);
 
 /********** CURD operation on comments**********/
 
 // create comment on post - api/posts/:postid/comments
-router.post('/posts/:postid/comment',Comment.createComment);//ok
+router.post('/posts/:postid/comment',Comment.createComment);
 
 // view all the comment on single post -api/posts/postid
-router.get('/posts/:postid/postComments',Comment.commentsOnpost);//ok
+router.get('/posts/:postid/postComments',Comment.commentsOnpost);
 
 // delete comment on post -api/posts/postid/comments/commentid
-router.post('/posts/:postid/comment/:commentid',Comment.commentDelete);//ok
+router.post('/posts/:postid/comment/:commentid',Comment.commentDelete);
 
 // update comment on the post by author -api/posts/postid/commentid
 router.post('/comment/:commentid/update' ,passport.authenticate('jwt', {session: false}),Comment.commentsUpdates);
