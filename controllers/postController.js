88bb5c92f3576f16c4e73a32c98c977a7a7730f8
ikram mohideen <ikram432k@gmail.com>
2022-11-12 12:41:32 +0530

    postController  updated

--- controllers/postController.js
+++ controllers/postController.js
@@ -52,7 +52,7 @@ exports.postOfUser =async(req,res,next)=>{
     try{
         const posts = await Post.find({author: req.params.userid}).populate('author', {username: 1, _id: 0});
         if(!posts || posts.length==0){
-            return res.status(403).json({message: "no posts available"})
+            return res.status(200).json({message: "no posts available"})
         }
         return res.status(200).json({posts});
     }catch(error){
