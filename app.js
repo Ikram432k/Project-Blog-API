7f938be024c5cb00de6ce130121ecbd33e31f3ab
ikram mohideen <ikram432k@gmail.com>
2022-11-12 08:19:49 +0530

    app.js updated

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 var app = express();
 
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin:['http://localhost:3000','https://web-production-9701.up.railway.app'], 
   // credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
@@ -38,7 +38,6 @@ const corsOptions ={
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -47,6 +46,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.options('*', cors(corsOptions));
+
+app.use(cors(corsOptions));
 
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
